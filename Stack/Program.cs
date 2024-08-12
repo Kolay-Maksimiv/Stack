dc659c78225e8fd8cecf2c38b22ff5b13959dec8
Mykola Maksymiv <kolaymaksimiv@gmail.com>
2024-08-12 13:46:38 +0300

    Implement Stack

--- Stack/Program.cs
+++ Stack/Program.cs
@@ -1,10 +1,20 @@
 ﻿namespace Stack
 {
-    internal class Program
+    internal partial class Program
     {
         static void Main(string[] args)
         {
-            
+            Stack stack = new Stack();
+
+            int ini = 0;
+            int max = 5;
+
+            for (int i = ini; i <= max; i++)
+                stack.Push(i);
+
+            for (int i = ini; i <= max ; i++)
+                Console.WriteLine(stack.Pop());
+
         }
     }
 }
